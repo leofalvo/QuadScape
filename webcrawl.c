b27f0b9be5e3022aa4ea8bc0022436363320dcdb
lfalvo <lfalvo@1804ADAMS.leofalvo.com>
2024-06-10 16:13:42 -0400

    Fixed some errors

--- webcrawl.c
+++ webcrawl.c
@@ -9,10 +9,10 @@
 struct MemoryStruct {
   char *memory;
   size_t size;
-}
+};
 
-static size_t WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *data){
-  size_t = realsize = size * nmemb;
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp){
+  size_t realsize = size * nmemb;
   struct MemoryStruct *mem = (struct MemoryStruct *)userp;
 
   char *ptr = realloc(mem->memory, mem->size + realsize +  1);
